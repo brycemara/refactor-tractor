3e4beabe901bbf5035c0979e110f4540f19191c9
Bryce Mara Jarrett <brycej98@gmail.com>
2020-10-23 20:45:33 -0500

    Add spacing

--- src/User-repo.js
+++ src/User-repo.js
@@ -2,12 +2,15 @@ class UserRepo {
   constructor(users) {
     this.users = users;
   };
+
   getDataFromID(id) {
     return this.users.find((user) => id === user.id);
   };
+
   getDataFromUserID(id, dataSet) {
     return dataSet.filter((userData) => id === userData.userID);
   };
+
   calculateAverageStepGoal() {
     const totalStepGoal = this.users.reduce((sumSoFar, data) => {
       return sumSoFar += data.dailyStepGoal;
@@ -15,29 +18,35 @@ class UserRepo {
     const averageSteps = totalStepGoal / this.users.length;
     return averageSteps;
   };
+
   makeSortedUserArray(id, dataSet) {
     let selectedID = this.getDataFromUserID(id, dataSet)
     let sortedByDate = selectedID.sort((a, b) => new Date(b.date) - new Date(a.date));
     return sortedByDate;
-  }
+  };
+
   getToday(id, dataSet) {
     return this.makeSortedUserArray(id, dataSet)[0].date;
   };
+
   getFirstWeek(id, dataSet) {
     return this.makeSortedUserArray(id, dataSet).slice(0, 7);
   };
+
   getWeekFromDate(date, id, dataSet) {
     let user = this.makeSortedUserArray(id, dataSet).find((sortedItem) => (sortedItem.date === date));
     let dateIndex = this.makeSortedUserArray(id, dataSet).indexOf(user);
     let week = this.makeSortedUserArray(id, dataSet).slice(dateIndex, dateIndex + 7);
     return week;
   };
+
   chooseWeekDataForAllUsers(dataSet, date) {
     let allUserData = dataSet.filter((dataItem) => {
       return (new Date(date)).setDate((new Date(date)).getDate() - 7) <= new Date(dataItem.date) && new Date(dataItem.date) <= new Date(date);
     });
     return allUserData;
   };
+
   chooseDayDataForAllUsers(dataSet, date) {
     let userData = dataSet.filter((dataItem) => {
       return dataItem.date === date
@@ -81,7 +90,7 @@ class UserRepo {
       return rankedAverages;
     }, []);
     return rankedUsersAndAverages;
-  }
+  };
 }
 
 export default UserRepo;
