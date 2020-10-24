fd2b9b2b092000c4d37c778406816925d77a5b8d
Bryce Mara Jarrett <brycej98@gmail.com>
2020-10-23 21:19:22 -0500

    Refactor user.js and user-test.js

--- src/User.js
+++ src/User.js
@@ -7,14 +7,15 @@ class User {
     this.strideLength = userDetails.strideLength;
     this.dailyStepGoal = userDetails.dailyStepGoal;
     this.friends = userDetails.friends;
+  };
 
-  }
   getFirstName() {
     return this.name.split(' ', 1).join();
-  }
-  getFriendsNames(userStorage) {
-    return this.friends.map((friendId) => (userStorage.getDataFromID(friendId).name));
-  }
-}
+  };
+
+  getFriendsNames(dataSet) {
+    return this.friends.map((friendId) => (dataSet.getDataFromID(friendId).name));
+  };
+};
 
 export default User;
