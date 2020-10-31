fe204d1d9548ab2bc8749954a23c4c1e4a608bd8
Allison Dietz <dietz.allison@outlook.com>
2020-10-31 14:06:58 -0600

    Add conditionals for successful input values in Fetch.API.js, add ARIA attributes to HTML tags

--- src/Fetch-API.js
+++ src/Fetch-API.js
@@ -42,11 +42,23 @@ let fetchApi = {
     .catch(error => console.log(error.message))
   },
   buildSleepObject() {
+    fetchApi.checkSleepInputs();
     let newData = {userID: parseInt(sleepIdInfo.value),
     date: sleepDateInfo.value,
     hoursSlept: parseInt(sleepHoursSleptInfo.value),
     sleepQuality: parseInt(sleepSleepQualityInfo.value)};
     return newData;
+  },
+  checkSleepInputs() {
+    if (sleepIdInfo.value < 1 || sleepIdInfo.value > 50) {
+      alert('Please input a valid User ID# between 1 and 50');
+    } else if (sleepDateInfo.value === '') {
+      alert('Please enter a valid date YYYY/MM/DD')
+    } else if (sleepHoursSleptInfo.value < 1) {
+      alert('I hope you slept longer than that...');
+    } else if (sleepSleepQualityInfo.value < 1) {
+      alert('Looks like you could use some rest!');
+    };
   },
   postHydrationData() {
    let newData = fetchApi.buildHydrationObject();
@@ -61,11 +73,23 @@ let fetchApi = {
    .catch(error => console.log(error.message))
  },
  buildHydrationObject() {
+   fetchApi.checkHydrationInputs();
    let newData = {userID: parseInt(hydrationIdInfo.value),
    date: hydrationDateInfo.value,
    numOunces: parseInt(hydrationOuncesInfo.value),
    };
    return newData;
+ },
+ checkHydrationInputs() {
+   if (hydrationIdInfo.value < 1 || hydrationIdInfo.value > 50) {
+     alert('Please input a valid User ID# between 1 and 50');
+   } else if (hydrationDateInfo.value === '') {
+     alert('Please enter a valid date YYYY/MM/DD')
+   } else if (hydrationOuncesInfo.value < 1) {
+     alert('Aren\'t you thirsty?');
+   } else if (hydrationOuncesInfo.value > 1000) {
+     alert('Are your kidneys ok?');
+   };
  },
  postActivityData() {
    let newData = fetchApi.buildActivityObject();
@@ -80,6 +104,15 @@ let fetchApi = {
    .catch(error => console.log(error.message))
  },
  buildActivityObject() {
+   if (activityIdInfo.value < 1 || activityIdInfo.value > 50) {
+     alert('Please input a valid User ID# between 1 and 50');
+   } else if (activityDateInfo.value === '') {
+     alert('Please enter a valid date YYYY/MM/DD')
+   } else if (activityNumStepsInfo.value < 1 || activityMinsActiveInfo.value < 1) {
+     alert('Get moving!');
+   } else if (activityFlightsOfStairsInfo.value < 1) {
+     alert('Get climbing!');
+   };
    let newData = {userID: parseInt(activityIdInfo.value),
    date: activityDateInfo.value,
    numSteps: parseInt(activityNumStepsInfo.value),
